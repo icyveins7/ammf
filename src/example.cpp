accc6235e0b84e2af80e82e55ed0e71407addd25
icyveins7 <icyveins@hotmail.com>
2025-05-14 11:06:17 +0800

    add cmd line args and clearer comparisons, refactor replace()

--- src/example.cpp
+++ src/example.cpp
@@ -1,3 +1,5 @@
+#include <cstdlib>
+#include <string>
 #include <vector>
 #include <algorithm>
 #include <iostream>
@@ -17,38 +19,66 @@ template <typename T>
 class VectorMedianFilter : public std::vector<T>
 {
 public:
+  // Inherit all constructors
+  using std::vector<T>::vector;
+
   void replace(const T& in, const T& out){
-    for (auto it = begin(); it != end(); ++it){
-      if (*it == out)
-        *it = in;
+    for (size_t i = 0; i < this->size(); ++i){
+      if (this->at(i) == out){
+        this->at(i) = in;
+        printf("Replaced at index %zd\n", i);
+      }
     }
   }
 };
 
 
-int main()
+int main(int argc, char* argv[])
 {
   printf("ammf\n");
 
-  std::vector<int> x = {1, 7, 5, 12};
-  for (int i = 0; i < 2; ++i){
+  size_t len = 4;
+  size_t medlen = 3;
+  if (argc >= 2)
+    len = (size_t)std::stoi(argv[1]);
+  if (argc >= 3)
+    medlen = (size_t)std::stoi(argv[2]);
+  printf("Using length %zd\n", len);
+  printf("Using median length %zd\n", medlen);
+  size_t iterations = len - medlen + 1;
+
+  std::vector<int> x(len);
+  // fill random values
+  for (auto& v : x){
+    v = std::rand() % 10;
+    printf("%d ", v);
+  }
+  printf("\n");
+
+
+  printf("======= just nth_element \n");
+  for (int i = 0; i < iterations; ++i){
     std::vector<int> y(
       x.begin() + i,
-      x.begin() + i + 3
+      x.begin() + i + medlen
     );
 
     simpleMedian(y);
-    printf("%d\n", y.at(1));
+    printf("Median: %d\n", y.at(medlen/2));
   }
 
   printf("======= vector median filter\n");
-  VectorMedianFilter<int> vmf(x.begin(), x.begin()+3);
+  VectorMedianFilter<int> vmf(x.begin(), x.begin()+medlen);
 
-  for (int i = 0; i < 2; ++i){
-    if (i > 1)
-      vmf.replace(x.at(i + 2), x.at(i - 1));
+  for (int i = 0; i < iterations; ++i){
+    if (i >= 1)
+      vmf.replace(x.at(i + medlen-1), x.at(i - 1));
+    for (int j = 0; j < vmf.size(); ++j)
+      printf("%d ", vmf.at(j));
+    printf("\n");
+
     simpleMedian(vmf);
-    printf("%d\n", vmf.at(1));
+    printf("Median: %d\n", vmf.at(medlen/2));
   }
 
   return 0;
