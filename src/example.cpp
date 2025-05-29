cd8c55721b699a76e5f81d0a84a0c8580e26e928
icyveins7 <icyveins@hotmail.com>
2025-05-29 14:12:15 +0800

    correct errors in VectorMedianFilter and LinkedList variants
    now use switch statement for all 9 cases in linked list variant, can
    optimise later if necessary

--- src/example.cpp
+++ src/example.cpp
@@ -11,145 +11,125 @@ class LinkedListMedian : public std::list<T>
 public:
   using std::list<T>::list;
 
-  void insertSorted(const T val){
-    // we cannot insert once we have settled on the median length
-    if (m_medianLength >= 0)
-      throw std::runtime_error("cannot insert once median length fixed. use replace instead");
-
-
-    if (this->size() == 0) {
-      this->push_back(val);
-      return;
-    }
-    else {
-      // walk to find insertion point
-      for (auto it = this->begin(); it != this->end(); ++it) {
-        if (*it >= val) {
-          // insert before this
-          this->insert(it, val);
-          return;
-        }
+  template <typename Iterator>
+  void insertSorted(const T val, Iterator begin, Iterator end){
+    for (auto it = begin; it != end; ++it){
+      if (*it >= val){
+        this->insert(it, val);
+        return;
       }
     }
     // if we haven't found insertion then push to end
-    this->push_back(val);
+    this->insert(end, val);
   }
 
-  void replace(const T toIns, const T toRem){
+  template <typename Iterator>
+  void eraseFirstOccurrence(const T val, Iterator begin, Iterator end){
+    for (auto it = begin; it != end; ++it){
+      if (*it == val){
+        this->erase(it);
+        return;
+      }
+    }
+    // If we didn't find one, throw
+    throw std::runtime_error("value not found in list for eraseFirstOccurrence");
+  }
+
+  void replace(const T newV, const T oldV){
     if (m_medianLength < 0)
       throw std::runtime_error("must call median() at least once to finalise median length before replacing");
 
-    std::cout << "Removing " << toRem << std::endl;
-    std::cout << "Inserting " << toIns << std::endl;
+    std::cout << "Removing " << oldV << std::endl;
+    std::cout << "Inserting " << newV << std::endl;
     // convention is to use geq
+    constexpr unsigned char NEW_IS_HIGHER = 0b00100000;
+    constexpr unsigned char NEW_IS_EQUAL  = 0b00010000;
+    constexpr unsigned char NEW_IS_LOWER  = 0b00001000;
+    constexpr unsigned char OLD_IS_HIGHER = 0b00000100;
+    constexpr unsigned char OLD_IS_EQUAL  = 0b00000010;
+    constexpr unsigned char OLD_IS_LOWER  = 0b00000001;
+    unsigned char state = 0;
     const T oldMed = this->median();
-    const bool insIsHigher = toIns >= oldMed;
-    const bool remIsHigher = toRem >= oldMed;
-    printf("inserted >= median ? %s\n", insIsHigher ? "true" : "false");
-    printf("removed >= median ? %s\n", remIsHigher ? "true" : "false");
-    bool inserted = false, removed = false;
-    // 4 cases
-    // a. both are below median, median does not change
-    if (!insIsHigher && !remIsHigher) {
-      for (auto it = this->begin(); it != m_medianIt;){
-        if (!inserted && *it >= toIns){
-          inserted = true;
-          this->insert(it, toIns);
-          std::cout << "Inserting before " << *it << std::endl;
-          ++it;
-        }
-        if (!removed && *it == toRem){
-          removed = true;
-          it = this->erase(it);
-        }
-        if (inserted && removed)
-          break; // early exit if possible
+    if (newV > oldMed)
+      state |= NEW_IS_HIGHER;
+    else if (newV == oldMed)
+      state |= NEW_IS_EQUAL;
+    else
+      state |= NEW_IS_LOWER;
+
+    if (oldV > oldMed)
+      state |= OLD_IS_HIGHER;
+    else if (oldV == oldMed)
+      state |= OLD_IS_EQUAL;
+    else
+      state |= OLD_IS_LOWER;
+
+    // There are 9 total cases
+    switch (state)
+    {
+      // Both of these do not involve median changes
+      case NEW_IS_HIGHER | OLD_IS_HIGHER: {
+        this->insertSorted(newV, std::next(this->m_medianIt), this->end());
+        this->eraseFirstOccurrence(oldV, std::next(this->m_medianIt), this->end());
+        break;
+      }
+      case NEW_IS_LOWER  | OLD_IS_LOWER: {
+        this->insertSorted(newV, this->begin(), this->m_medianIt);
+        this->eraseFirstOccurrence(oldV, this->begin(), this->m_medianIt);
+        break;
+      }
+      // Both of these involve median changes
+      case NEW_IS_HIGHER | OLD_IS_LOWER: {
+        this->insertSorted(newV, std::next(this->m_medianIt), this->end());
+        this->eraseFirstOccurrence(oldV, this->begin(), this->m_medianIt);
+        // Move median forward
+        this->m_medianIt = std::next(this->m_medianIt);
+        break;
+      }
+      case NEW_IS_LOWER  | OLD_IS_HIGHER: {
+        this->insertSorted(newV, this->begin(), this->m_medianIt);
+        this->eraseFirstOccurrence(oldV, std::next(this->m_medianIt), this->end());
+        // Move median backward
+        this->m_medianIt = std::prev(this->m_medianIt);
+        break;
+      }
+      // When new is equal to median, we can choose insert location
+      // so as to not invalidate the median iterator
+      case NEW_IS_EQUAL  | OLD_IS_LOWER: {
+        // Insert before the current median
+        this->insert(m_medianIt, newV);
+        this->eraseFirstOccurrence(oldV, this->begin(), this->m_medianIt);
+        break;
+      }
+      case NEW_IS_EQUAL  | OLD_IS_HIGHER: {
+        // Insert after the median
+        this->insert(std::next(m_medianIt), newV);
+        this->eraseFirstOccurrence(oldV, std::next(this->m_medianIt), this->end());
+        break;
+      }
+      // When old is equal to median, we need to invalidate median iterator
+      case NEW_IS_LOWER  | OLD_IS_EQUAL: {
+        this->insertSorted(newV, this->begin(), this->m_medianIt);
+        this->m_medianIt = this->erase(this->m_medianIt);
+        // Move median back
+        this->m_medianIt = std::prev(this->m_medianIt);
+        break;
+      }
+      case NEW_IS_HIGHER | OLD_IS_EQUAL: {
+        this->insertSorted(newV, std::next(this->m_medianIt), this->end());
+        this->m_medianIt = this->erase(this->m_medianIt);
+        // No need to move the median, it is already correct
+        break;
+      }
+      // This is a trivial case, no need to do anything at all
+      case NEW_IS_EQUAL | OLD_IS_EQUAL: {
+        break;
+      }
+      default: {
+        throw std::runtime_error("Invalid state in replace()\n");
       }
 
-      // if not yet inserted, insert
-      if (!inserted)
-        this->insert(m_medianIt, toIns);
-      // if not yet removed, remove
-      if (!removed) // TODO: maybe check if it's equal?
-        this->erase(m_medianIt);
     }
-
-    // b. both are above median, median does not change
-    else if (insIsHigher && remIsHigher) {
-      for (auto it = m_medianIt; it != this->end();){
-        if (!inserted && *it >= toIns){
-          inserted = true;
-          this->insert(it, toIns);
-          ++it;
-        }
-        if (!removed && *it == toRem){
-          removed = true;
-          it = this->erase(it);
-        }
-        if (inserted && removed)
-          break; // early exit if possible
-      }
-
-      // if not yet inserted, insert
-      if (!inserted)
-        this->insert(m_medianIt, toIns);
-      // if not yet removed, remove
-      if (!removed) // TODO: maybe check if it's equal?
-        this->erase(m_medianIt);
-    }
-
-    // c. new is below, old is above, median must be decremented
-    else if (!insIsHigher && remIsHigher) {
-      // insert below
-      for (auto it = this->begin(); it != m_medianIt; ++it){
-        if (*it >= toIns){
-          this->insert(it, toIns);
-          break;
-        }
-      }
-      // if not yet inserted, insert
-      if (!inserted)
-        this->insert(m_medianIt, toIns);
-      // remove above
-      for (auto it = m_medianIt; it != this->end(); ++it){
-        if (*it == toRem){
-          it = this->erase(it);
-          break;
-        }
-      }
-      // if not yet removed, remove
-      if (!removed) // TODO: maybe check if it's equal?
-        this->erase(m_medianIt);
-      // move median back
-      m_medianIt--;
-    }
-
-    // d. new is above, old is below, median must be incremented
-    else if (insIsHigher && !remIsHigher) {
-      // remove below
-      for (auto it = this->begin(); it != m_medianIt; ++it){
-        if (*it == toRem){
-          this->erase(it);
-          break;
-        }
-      }
-      // if not yet removed, remove
-      if (!removed) // TODO: maybe check if it's equal?
-        this->erase(m_medianIt);
-      // insert above
-      for (auto it = m_medianIt; it != this->end(); ++it){
-        if (*it >= toIns){
-          this->insert(it, toIns);
-          break;
-        }
-      }
-      // if not yet inserted, insert
-      if (!inserted)
-        this->insert(m_medianIt, toIns);
-      // move median forward
-      m_medianIt++;
-    }
-
   }
 
   T median(){
@@ -194,6 +174,7 @@ public:
       if (this->at(i) == out){
         this->at(i) = in;
         printf("Replaced at index %zd\n", i);
+        break;
       }
     }
   }
@@ -235,6 +216,9 @@ int main(int argc, char* argv[])
   std::vector<int> x = quickrandvec(len);
   quickprint(x.begin(), x.end());
 
+  std::vector<int> truth(iterations);
+
+  // benchmark ground truth
   printf("======= just nth_element \n");
   for (int i = 0; i < iterations; ++i){
     std::vector<int> y(
@@ -245,6 +229,7 @@ int main(int argc, char* argv[])
     quickprint(y.begin(), y.end());
     simpleMedian(y);
     printf("Median: %d\n", y.at(medlen/2));
+    truth.at(i) = y.at(medlen/2);
   }
 
   printf("======= vector median filter\n");
@@ -258,6 +243,9 @@ int main(int argc, char* argv[])
 
     simpleMedian(vmf);
     printf("Median: %d\n", vmf.at(medlen/2));
+    if (truth.at(i) != vmf.at(medlen/2)){
+      printf("ERROR: %d != %d\n", truth.at(i), vmf.at(medlen/2));
+    }
   }
 
   printf("======= linked list derived class\n");
@@ -279,6 +267,9 @@ int main(int argc, char* argv[])
 
     int med = llmf.median();
     printf("Median: %d\n", med);
+    if (truth.at(i) != med){
+      printf("ERROR: %d != %d\n", truth.at(i), med);
+    }
   }
   return 0;
 }
